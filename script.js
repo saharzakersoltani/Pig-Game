b3517136f266827e4279c44557f492e2e4379e3c
ManyToMany <sahar.zs2002@gmail.com>
2025-05-09 13:34:39 +0330

    edit 1

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 'use strict';
 //  Selecting elements:
-const scoreEl = document.querySelectorAll('.score');
+const scoreEl0 = document.querySelector('#score--0');
+const scoreEl1 = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const currentEl0 = document.querySelector('#current--0');
+const currentEl1 = document.querySelector('#current--1');
 
 // to make scores 0 (starting condition)
-for (let i = 0; i < scoreEl.length; i++) {
-  scoreEl[i].textContent = 0;
-}
+scoreEl0.textContent = '0';
+scoreEl1.textContent = '0';
 
 // to make the dice hidden (strting conditions)
 diceEl.classList.add('hidden');
 
 // click on roll dice button (Rolling dice functionality)
-let diceElImage = document.createElement('diceEl');
-
+let currentScore = 0;
 btnRoll.addEventListener('click', function () {
   const randomNum = Math.floor(Math.random() * 6 + 1);
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${randomNum}.png`;
   console.log(randomNum);
 
+  if (randomNum !== 1) {
+    currentScore = currentScore + randomNum;
+    currentEl0.textContent = currentScore;
+  } else {
+  }
   /*
   // The second way to show dice image based on the rolling 😐
   switch (randomNum) {
